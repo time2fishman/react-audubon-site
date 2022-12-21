96eca6116580598a0c597c82c58f660e03796e94
time2fishman <adam_h_martinez@hotmail.com>
2022-12-21 14:46:17 -0700

    displaying dog pics and names on cards

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -2,26 +2,42 @@ import React, { useState, useEffect } from "react";
 
 const Dogs = () => {
   const [dogs, setDogs] = useState([]);
+  // console.log(dogs);
 
   useEffect(() => {
-    const url = `https://api.api-ninjas.com/v1/dogs?name=poodle`;
+    const url = `https://api.api-ninjas.com/v1/dogs?trainability=5`;
     const apiKey = process.env.REACT_APP_API_KEY;
     fetch(url, {
       headers: new Headers({
-        "x-api-key": `${apiKey}`,
+        "x-api-key": apiKey,
       }),
     })
       .then((res) => res.json())
       .then((json) => {
+        // console.log(json)
         setDogs(json);
-        console.log(dogs);
-      });
+      })
+      .catch(console.error)
   }, []);
 
   return (
-    <div>
-      <h1>Dogs component</h1>
-    </div>
+    <section className="container">
+      {dogs.map((dog, index) => {
+        return (
+          <div className="card" key={index}>
+          <div className="card-image">
+            <img
+              src={dog.image_link}
+              alt={dog.name}
+              />
+          </div>
+          <div className="card-title">
+            <h3>{dog.name}</h3>
+          </div>
+        </div>
+        )
+      })}
+    </section>
   );
 };
 
